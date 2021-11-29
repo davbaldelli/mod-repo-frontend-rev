2dbee938310e17752246bbd16413f3356e036aa6
Davide Baldelli <davbaldelli@gmail.com>
2021-11-29 10:48:27 +0100

    added 404 and skeleton to detail pages

--- src/components/cars/CarDetail.vue
+++ src/components/cars/CarDetail.vue
@@ -2,34 +2,75 @@
   <div class="p-grid">
     <div class="p-col-12 p-lg-2 p-xl-4"></div>
     <div class="p-col-12 p-lg-8 p-xl-4">
-      <div v-if="this.car" class="p-grid">
-        <div class="p-col-12">
-          <img :src="car.Image" alt="car thumbnail" class="rounded-3 card-img">
-        </div>
-        <div class="p-col-12">
+      <div class="p-grid">
+        <div v-if="car" class="p-col-12">
           <div class="p-grid">
-            <div class="p-col-12 ">
-              <h1 class="display-4">{{`${car.Brand.Name} ${car.ModelName}`}}</h1>
+            <div class="p-col-12">
+              <img :src="car.Image" alt="car thumbnail" class="rounded-3 card-img">
             </div>
-            <div class="p-col-12 p-p-3">
-              <div class="p-grid p-card">
-                <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-3 p-pb-0">
-                  <strong>Model Name</strong> {{car.ModelName}}<hr>
-                  <strong>Brand</strong> {{car.Brand.Name}}<hr>
-                  <strong>Nation</strong> {{car.Brand.Nation.Name}}<hr>
-                  <strong>Year</strong> {{car.Year}}<hr>
-                  <strong>Author</strong> <a :href="car.Author.Link">{{car.Author.Name}}</a><hr>
+            <div class="p-col-12">
+              <div class="p-grid">
+                <div class="p-col-12">
+                  <h1 class="display-4">{{`${car.Brand.Name} ${car.ModelName}`}}</h1>
                 </div>
-                <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-0">
-                  <strong>Power</strong> {{car.BHP}} BHP<hr>
-                  <strong>Torque</strong> {{car.Torque}} Nm<hr>
-                  <strong>Max Speed</strong> {{car.TopSpeed}} Km/h<hr>
-                  <strong>Drivetrain</strong> {{car.Drivetrain}}<hr>
-                  <strong>Transmission</strong> {{car.Transmission}}<hr>
+                <div class="p-col-12 p-p-3">
+                  <div class="p-grid p-card">
+                    <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-3 p-pb-0">
+                      <strong>Model Name</strong> {{car.ModelName}}<hr>
+                      <strong>Brand</strong> {{car.Brand.Name}}<hr>
+                      <strong>Nation</strong> {{car.Brand.Nation.Name}}<hr>
+                      <strong>Year</strong> {{car.Year}}<hr>
+                      <strong>Author</strong> <a :href="car.Author.Link">{{car.Author.Name}}</a><hr>
+                    </div>
+                    <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-0">
+                      <strong>Power</strong> {{car.BHP}} BHP<hr>
+                      <strong>Torque</strong> {{car.Torque}} Nm<hr>
+                      <strong>Max Speed</strong> {{car.TopSpeed}} Km/h<hr>
+                      <strong>Drivetrain</strong> {{car.Drivetrain}}<hr>
+                      <strong>Transmission</strong> {{car.Transmission}}<hr>
+                    </div>
+                  </div>
                 </div>
+
               </div>
             </div>
             <div class="p-col-12"><Button style="width: 100%" label="Download" @click="openInNewTab(car.DownloadLink)"></Button></div>
+          </div>
+        </div>
+        <div v-if="!car && !carLoading" class="p-col-12">
+          <h3 class="p-mt-4">I'm sorry, but I can't find the car that you are looking for. You can turn back to the <router-link to="/cars/">cars list</router-link> and see if that exits.</h3>
+        </div>
+      </div>
+      <div v-if="carLoading" class="p-col-12">
+        <div class="p-grid">
+          <div class="p-col-12">
+            <Skeleton height="450px" width="100%"></Skeleton>
+          </div>
+          <div class="p-col-12">
+            <div class="p-grid">
+              <div class="p-col-12">
+                <Skeleton height="60px" width="70%"></Skeleton>
+              </div>
+              <div class="p-col-12 p-p-3">
+                <div class="p-grid p-card">
+                  <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-3 p-pb-0">
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                  </div>
+                  <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-0">
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                  </div>
+                </div>
+              </div>
+              <div class="p-col-12"><Skeleton class="p-p-2" height="40px"></Skeleton></div>
+            </div>
           </div>
         </div>
       </div>
@@ -41,11 +82,13 @@
 <script>
 
 import Button from "primevue/button";
+import Skeleton from "primevue/skeleton";
 
 export default {
   name: "CarDetail",
   components:{
     Button,
+    Skeleton,
   },
   data(){
     return{
@@ -56,6 +99,9 @@ export default {
     car(){
       return this.$store.getters['cars/getCarById'](this.id)
     },
+    carLoading(){
+      return this.$store.getters['cars/loadingCars']
+    }
   },
   mounted() {
     this.init()
--- src/components/tracks/TrackDetail.vue
+++ src/components/tracks/TrackDetail.vue
@@ -2,40 +2,83 @@
   <div class="p-grid">
     <div class="p-col-12 p-lg-2 p-xl-4"></div>
     <div class="p-col-12 p-lg-8 p-xl-4">
-      <div v-if="track" class="p-grid">
-        <div class="p-col-12">
-          <div class="p-grid">
-            <div class="p-col-12">
-              <img :src="track.Image" alt="track thumbnail" class="rounded-3 card-img">
-            </div>
-            <div class="p-col-12">
-              <h1 class="display-4">{{track.Name}}</h1>
-            </div>
-            <div class="p-col-12 p-p-3">
-              <div class="p-grid p-card p-p-2">
-                <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-3 p-pb-0">
-                  <strong>Name</strong>: {{ track.Name }}<hr>
-                  <strong>Year</strong>: {{track.Year}}<hr>
-                </div>
-                <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-0">
-                  <strong>Location</strong>: {{ track.Location }}, {{ track.Nation.Name }}<hr>
-                  <strong>Author</strong>: <a :href="track.Author.Link">{{ track.Author.Name }}</a><hr>
+      <div v-if="!track && !trackLoading" class="p-col-12">
+        <h3 class="p-mt-4">I'm sorry, but I can't find the track that you are looking for. You can turn back to the <router-link to="/tracks/">tracks list</router-link> and see if that exits.</h3>
+      </div>
+      <div v-if="trackLoading">
+        <div class="p-grid">
+          <div class="p-col-12">
+            <Skeleton height="450px" width="100%"></Skeleton>
+          </div>
+          <div class="p-col-12">
+            <div class="p-grid">
+              <div class="p-col-12">
+                <Skeleton height="60px" width="70%"></Skeleton>
+              </div>
+              <div class="p-col-12 p-p-3">
+                <div class="p-grid p-card">
+                  <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-3 p-pb-0">
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                  </div>
+                  <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-0">
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                    <Skeleton class="p-m-2" height="30px" width="100%"></Skeleton>
+                  </div>
                 </div>
               </div>
+              <div class="p-col-12 p-mt-2">
+                <Skeleton width="30%" height="40px"></Skeleton>
+              </div>
+              <div class="p-col-12 p-p-3">
+                <div class="p-grid p-card">
+                  <div class="p-col-12 p-sm-4"><Skeleton height="30px" width="40%"/></div>
+                  <div class="p-col-12 p-sm-4"><Skeleton height="30px" width="60%"/></div>
+                  <div class="p-col-12 p-sm-4"><Skeleton height="30px" width="50%"/></div>
+                </div>
+              </div>
+              <div class="p-col-12"><Skeleton class="p-p-2" height="40px"></Skeleton></div>
             </div>
           </div>
         </div>
-        <div class="p-col-12 p-mt-2"><h3>Layouts</h3></div>
-        <div class="p-col-12">
-          <div class="p-grid p-p-2">
-            <div v-for="(layout, id) in track.Layouts" :key="id" class="p-col-12  p-p-2">
-              <div class="p-grid p-card">
-                <div class="p-col-12 p-sm-4"><strong>Name</strong>: {{ layout.Name }}</div>
-                <div class="p-col-12 p-sm-4"><strong>Type</strong>: {{ layout.Category }}</div>
-                <div class="p-col-12 p-sm-4"><strong>Length</strong>: {{ layout.LengthM }}m</div>
+      </div>
+      <div v-if="track" class="p-col-12">
+        <div class="p-grid">
+          <div class="p-col-12">
+            <div class="p-grid">
+              <div class="p-col-12">
+                <img :src="track.Image" alt="track thumbnail" class="rounded-3 card-img">
+              </div>
+              <div class="p-col-12">
+                <h1 class="display-4">{{track.Name}}</h1>
+              </div>
+              <div class="p-col-12 p-p-3">
+                <div class="p-grid p-card p-p-2">
+                  <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-3 p-pb-0">
+                    <strong>Name</strong>: {{ track.Name }}<hr>
+                    <strong>Year</strong>: {{track.Year}}<hr>
+                  </div>
+                  <div class="p-col-12 p-sm-6 p-p-sm-3 p-pt-0">
+                    <strong>Location</strong>: {{ track.Location }}, {{ track.Nation.Name }}<hr>
+                    <strong>Author</strong>: <a :href="track.Author.Link">{{ track.Author.Name }}</a><hr>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <div class="p-col-12 p-mt-2"><h3>Layouts</h3></div>
+          <div class="p-col-12">
+            <div class="p-grid p-p-2">
+              <div v-for="(layout, id) in track.Layouts" :key="id" class="p-col-12  p-p-2">
+                <div class="p-grid p-card">
+                  <div class="p-col-12 p-sm-4"><strong>Name</strong>: {{ layout.Name }}</div>
+                  <div class="p-col-12 p-sm-4"><strong>Type</strong>: {{ layout.Category }}</div>
+                  <div class="p-col-12 p-sm-4"><strong>Length</strong>: {{ layout.LengthM }}m</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-col-12"><Button style="width: 100%" label="Download" @click="openInNewTab(track.DownloadLink)"></Button></div>
         </div>
       </div>
     </div>
@@ -44,8 +87,15 @@
 </template>
 
 <script>
+import Skeleton from "primevue/skeleton";
+import Button from "primevue/button";
+
 export default {
   name: "TrackDetail",
+  components:{
+    Skeleton,
+    Button,
+  },
   data() {
     return {
       name: this.$route.params.name
@@ -54,7 +104,10 @@ export default {
   computed: {
     track() {
       return this.$store.getters['tracks/getTrackByName'](this.name)
-    }
+    },
+    trackLoading(){
+      return this.$store.getters['tracks/loadingTracks']
+    },
   },
   mounted() {
     this.init()
@@ -62,7 +115,10 @@ export default {
   methods: {
     init() {
       this.$store.dispatch('tracks/getAllTracks')
-    }
+    },
+    openInNewTab(url) {
+      window.open(url, '_blank').focus();
+    },
   },
 }
 </script>
